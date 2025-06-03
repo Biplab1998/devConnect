79b90fabc639d4edbd095f8bd017c1cfbb3780cc
Biplab1998 <bittupaul1998@gmail.com>
2025-06-03 09:06:39 +0530

    explored routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use("/dashboard", (req, res) => {
-  res.send(" Hi this is Dashboard");
+app.use("/user", (req, res) => {
+  res.send("Heheheheheeeeeeeeeeeeeeeeeeeeeee");
+});
+
+app.get("/user", (req, res) => {
+  res.send({
+    name: "Lucky singh",
+    place: "Kashmir",
+  });
+});
+
+app.post("/user", (req, res) => {
+  console.log("check request", req.data);
+  res.send("Your data added successfully");
 });
 
 app.use("/check-json", (req, res) => {
@@ -13,6 +25,14 @@ app.use("/check-json", (req, res) => {
     mobile: "+91 7004466712",
   });
 });
+
+app.use("/dashboard", (req, res) => {
+  res.send(" Hi this is Dashboard");
+});
+
+// app.use("/", (req, res) => {
+//   res.send("Main page with / route");
+// });
 
 app.listen(3001, () => {
   console.log("Yeah, the server is running!");
