6b40e4b33f433217ba4dee7058909ec464438c47
Biplab1998 <bittupaul1998@gmail.com>
2025-06-08 14:18:20 +0530

    exploring params and queries

--- src/app.js
+++ src/app.js
@@ -2,36 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-  res.send("Heheheheheeeeeeeeeeeeeeeeeeeeeee");
+app.use("/user/:username/:pass/:place", (req, res) => {
+  console.log(req.query.user);
+  console.log(req.params);
+  res.send("This is the users page");
 });
 
-app.get("/user", (req, res) => {
-  res.send({
-    name: "Lucky singh",
-    place: "Kashmir",
-  });
-});
+// app.get("/user", (req, res) => {
+//   res.send({
+//     name: "Lucky singh",
+//     place: "Kashmir",
+//   });
+// });
 
-app.post("/user", (req, res) => {
-  console.log("check request", req.data);
-  res.send("Your data added successfully");
-});
+// app.post("/user", (req, res) => {
+//   console.log("check request", req.data);
+//   res.send("Your data added successfully");
+// });
 
-app.use("/check-json", (req, res) => {
-  res.json({
-    name: "Biplab Paul",
-    place: "Dhanbad",
-    mobile: "+91 7004466712",
-  });
-});
+// app.use("/check-json", (req, res) => {
+//   res.json({
+//     name: "Biplab Paul",
+//     place: "Dhanbad",
+//     mobile: "+91 7004466712",
+//   });
+// });
 
-app.use("/dashboard", (req, res) => {
-  res.send(" Hi this is Dashboard");
-});
-
-// app.use("/", (req, res) => {
-//   res.send("Main page with / route");
+// app.use("/dashboard", (req, res) => {
+//   res.send(" Hi this is Dashboard");
 // });
 
 app.listen(3001, () => {
